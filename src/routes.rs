0a5def7808b1eb24e05b46519053d7a17ab9bc56
Sean Smith <sean.smith@tenx.tech>
2019-02-07 23:42:04 +0800

    Rename to messenger_rocket

--- src/routes.rs
+++ src/routes.rs
@@ -6,16 +6,14 @@ use super::postgres;
 /// # Builder for Rocket server and routes
 /// Initializes database connection pool and route handlers and launches Rocket
 pub fn build_server() -> rocket::Rocket {
-    rocket::ignite()
-        .manage(postgres::init_pool())
-        .mount(
-            "/",
-            routes![
-                controllers::health_check,
-                controllers::get_messages,
-                controllers::save_message,
-                controllers::edit_message,
-                controllers::delete_message
-            ],
-        )
+    rocket::ignite().manage(postgres::init_pool()).mount(
+        "/",
+        routes![
+            controllers::health_check,
+            controllers::get_messages,
+            controllers::save_message,
+            controllers::edit_message,
+            controllers::delete_message
+        ],
+    )
 }
