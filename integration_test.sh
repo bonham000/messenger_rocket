3269f33ae00c52cbe9aa491b6c1c56ed74355b98
Sean Smith <sean.smith@tenx.tech>
2019-02-09 00:41:39 +0800

    Rename

--- integration_test.sh
+++ integration_test.sh
@@ -2,7 +2,7 @@
 
 pwd
 ls -a
-docker run -ti -v $PWD:/usr/src/root/project -w /usr/src/root/project rustlang/rust:nightly cargo build --release
+docker run -ti -v $PWD:/usr/src/project -w /usr/src/project rustlang/rust:nightly cargo build --release
 docker-compose up -d
 cd integration && cargo test -- --nocapture
 docker-compose down
