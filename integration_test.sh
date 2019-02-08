ebb3ab2870d96ffa17d6dcf973943c4a13654687
Sean Smith <sean.smith@tenx.tech>
2019-02-09 00:35:02 +0800

    Debugging

--- integration_test.sh
+++ integration_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pwd
+ls -a
 docker run -ti -v $PWD:/usr/src/messenger_rocket -w /usr/src/messenger_rocket rustlang/rust:nightly cargo build --release
 docker-compose up -d
 cd integration && cargo test -- --nocapture
