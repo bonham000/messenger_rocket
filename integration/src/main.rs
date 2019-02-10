7a5b3fda99d698a00c68aaf0de2d4ca5d8c1a973
Sean Smith <sean.smith@tenx.tech>
2019-02-11 00:19:23 +0800

    Add integration tests for messages REST API

--- integration/src/main.rs
+++ integration/src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate reqwest;
+extern crate serde_json;
 
 fn main() {
     // Integration tests for the Messenger Rocket server
@@ -10,6 +11,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
+    use std::vec::Vec;
     use std::{thread, time};
 
     #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
@@ -21,56 +23,193 @@ mod tests {
     }
 
     #[test]
-    fn health_check_endpoint() {
+    fn test_messages_rest_api() {
+        let client = reqwest::Client::new();
+
         wait_for_server();
-        let result = reqwest::get("http://0.0.0.0:8000/rocket");
+
+        let result = reqwest::get(&get_request_path("/rocket"));
         let expected = "Hello from Messenger Rocket server";
         assert_eq!(result.unwrap().text().unwrap(), expected);
-    }
 
-    #[test]
-    fn get_messages_is_empty_array_initially() {
-        wait_for_server();
-        let result = reqwest::get("http://0.0.0.0:8000/messages");
-        let expected = "[]";
-        assert_eq!(result.unwrap().text().unwrap(), expected);
-    }
-
-    #[test]
-    fn post_message_adds_messages() {
-        wait_for_server();
-        let result = reqwest::get("http://0.0.0.0:8000/messages");
-        let expected = "[]";
-        assert_eq!(result.unwrap().text().unwrap(), expected);
+        assert_message_history_is_empty();
 
         let message = String::from("Hello, from Earth");
         let author = String::from("Seanie X");
 
-        let mut test_message = HashMap::new();
-        test_message.insert("message", &message);
-        test_message.insert("author", &author);
+        let test_message = get_new_message(&author, &message);
 
-        let client = reqwest::Client::new();
         let response = client
-            .post("http://0.0.0.0:8000/message")
+            .post(&get_request_path("/message"))
             .json(&test_message)
             .send();
 
         let post_message_result: SavedMessage =
             serde_json::from_str(&response.unwrap().text().unwrap()).unwrap();
 
+        // Assert a new post message returns a matching message object
         assert_eq!(post_message_result.message, message);
         assert_eq!(post_message_result.author, author);
+
+        let message_list = fetch_messages();
+
+        // Assert the new message is returned by GET /messages
+        assert_eq!(
+            message_exists_in_list(message_list, &post_message_result),
+            true
+        );
+
+        let edit_message_text = String::from("Hello, from Venus");
+        let edited_message: SavedMessage = SavedMessage {
+            id: post_message_result.id,
+            uuid: post_message_result.uuid,
+            author: post_message_result.author,
+            message: edit_message_text.clone(),
+        };
+
+        let edit_response = client
+            .put(&get_request_path("/message"))
+            .json(&edited_message)
+            .send();
+
+        let edit_message_result: SavedMessage =
+            serde_json::from_str(&edit_response.unwrap().text().unwrap()).unwrap();
+
+        // Assert the edit message response returns the matching message object
+        assert_eq!(edit_message_result.message, edit_message_text);
+        assert_eq!(edit_message_result.author, author);
+
+        let edit_message_list = fetch_messages();
+
+        // Assert the edited message is now returned by GET /messages
+        assert!(
+            message_exists_in_list(edit_message_list, &edit_message_result),
+            true
+        );
+
+        // Verify the message can be delete
+        delete_by_message_id(edit_message_result.id);
+
+        assert_message_history_is_empty();
+
+        // Verify paginated fetching works
+        save_multiple_messages();
+
+        let result = fetch_messages();
+        assert_eq!(result.len(), 3);
+
+        let result = fetch_messages_paginated(1);
+        assert_eq!(result.len(), 1);
+
+        let result = fetch_messages_paginated(2);
+        assert_eq!(result.len(), 2);
+
+        let result = fetch_messages();
+
+        for m in result.iter() {
+            delete_by_message_id(m.id);
+        }
+
+        assert_message_history_is_empty();
     }
 
-    // Loop until server is ready
+    fn delete_by_message_id(id: i32) {
+        let client = reqwest::Client::new();
+
+        let id_str = id.to_string();
+        let delete_path = ["/message/", &id_str].concat();
+        let result = client.delete(&get_request_path(&delete_path)).send();
+
+        assert_eq!(
+            &result.unwrap().text().unwrap(),
+            "{\"status\":\"Message deleted!\"}"
+        );
+    }
+
+    fn save_multiple_messages() {
+        let client = reqwest::Client::new();
+
+        let message_list: Vec<HashMap<&str, &str>> = vec![
+            get_new_message("Seanie X", "Hello!!!"),
+            get_new_message("Seanie X", "Goodbye!!!"),
+            get_new_message("Seanie X", "Blah blah blah!!!"),
+        ];
+
+        let mut result;
+
+        for m in message_list.iter() {
+            result = client.post(&get_request_path("/message")).json(&m).send();
+
+            let post_message_result: SavedMessage =
+                serde_json::from_str(&result.unwrap().text().unwrap()).unwrap();
+
+            let text = (*m.get("message").unwrap()).to_string();
+            assert_eq!(post_message_result.message, text);
+        }
+    }
+
+    fn get_new_message<'a>(author: &'a str, message: &'a str) -> HashMap<&'a str, &'a str> {
+        let mut new_message = HashMap::new();
+        new_message.insert("message", message);
+        new_message.insert("author", author);
+        new_message
+    }
+
+    fn fetch_messages() -> Vec<SavedMessage> {
+        let result = reqwest::get(&get_request_path("/messages"));
+        let message_list = serde_json::from_str(&result.unwrap().text().unwrap()).unwrap();
+        message_list
+    }
+
+    fn fetch_messages_paginated(limit: i64) -> Vec<SavedMessage> {
+        let limit_str = limit.to_string();
+        let path = ["/messages?limit=", &limit_str].concat();
+
+        let result = reqwest::get(&get_request_path(&path));
+        let message_list = serde_json::from_str(&result.unwrap().text().unwrap()).unwrap();
+        message_list
+    }
+
+    fn message_exists_in_list(
+        message_list: Vec<SavedMessage>,
+        target_message: &SavedMessage,
+    ) -> bool {
+        let target_author = &target_message.author;
+        let target_message = &target_message.message;
+
+        for m in message_list.iter() {
+            if m.author == *target_author && m.message == *target_message {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn assert_message_history_is_empty() {
+        let result = reqwest::get(&get_request_path("/messages"));
+        let expected = "[]";
+        assert_eq!(result.unwrap().text().unwrap(), expected);
+    }
+
+    fn get_request_path(path: &str) -> String {
+        ["http://0.0.0.0:8000", path].concat()
+    }
+
+    // Loop until server is ready, or timeout
     fn wait_for_server() {
         let mut limit = 0;
         let maximum = 15;
-        let mut maybe_response = reqwest::get("http://0.0.0.0:8000/rocket");
+        let mut maybe_response = reqwest::get(&get_request_path("/rocket"));
         loop {
             match maybe_response {
                 Ok(_) => {
+                    let client = reqwest::Client::new();
+                    let result = client.delete(&get_request_path("/admin/delete")).send();
+                    assert_eq!(
+                        &result.unwrap().text().unwrap(),
+                        "{\"status\":\"All messages deleted\"}"
+                    );
                     println!("Rocket is ready - running tests!");
                     break;
                 }
@@ -84,7 +223,7 @@ mod tests {
                     if limit > maximum {
                         panic!("Maximum retries reached - aborting tests!");
                     } else {
-                        maybe_response = reqwest::get("http://0.0.0.0:8000/rocket");
+                        maybe_response = reqwest::get(&get_request_path("/messages"));
                     }
                 }
             }
