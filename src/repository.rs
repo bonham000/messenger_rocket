e998bb664a34c5bc0ca158229a4c51a79e87d291
Sean Smith <sean.smith@tenx.tech>
2019-02-11 15:03:51 +0800

    Order responses by id in GET /messages

--- src/repository.rs
+++ src/repository.rs
@@ -7,7 +7,7 @@ use super::types::{InsertableMessage, Message, SavedMessage};
 /// # Get messages
 /// Method to return recent messages
 pub fn get_messages(limit: i64, connection: &PgConnection) -> QueryResult<Vec<SavedMessage>> {
-    messages.limit(limit).load::<SavedMessage>(&*connection)
+    messages.limit(limit).order(id).load::<SavedMessage>(&*connection)
 }
 
 /// # Insert new message
